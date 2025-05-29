d3d49d89fbecec1c19c834ebe7ac821323bacfa0
veyis <vakgul@gmail.com>
2025-05-29 21:32:58 +0300

    v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
